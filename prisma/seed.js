9d057a3a7feeb8d6ec929753c3ef62af0d492f0a
Joy Harrington <joysyk23@gmail.com>
2024-10-15 11:01:07 -0500

    Create Restaurants and customers in seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,16 @@
 const prisma = require("../prisma");
 
-const seed = async () => {
-  // TODO: this function
+const seed = async (numRestaurants = 3, numCustomers = 5) => {
+  const restaurants = Array.from({ length: numRestaurants }, (_, i) => ({
+    name: `Restaurant ${i + 1}`,
+  }));
+  await prisma.restaurant.createMany({ data: restaurants });
+
+  const customers = Array.from({ length: numCustomers }, (_, i) => ({
+    name: `Customer ${i + 1}`,
+    email: `customer${i}@foo.bar`,
+  }));
+  await prisma.customer.createMany({ data: customers });
 };
 
 seed()
